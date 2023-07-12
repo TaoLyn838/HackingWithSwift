b66b837ec4bf830360ca607d5db88e78543ac237
Chengtao <chengtaolinctl@gmail.com>
2023-07-12 02:05:01 -0400

    Finish Project 5

---
+++ SwiftUI/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by CHENGTAO on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
